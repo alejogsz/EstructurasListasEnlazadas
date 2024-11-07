6b2afbd32db410cb39d999f43ab872eeb366de97
alejogsz <140736259+alejogsz@users.noreply.github.com>
2024-11-07 09:17:15 -0500

    Update paciente.py
    typehints y comentarios

--- listasenlazadas/paciente.py
+++ listasenlazadas/paciente.py
@@ -1,18 +1,17 @@
 class Paciente:
-    def __init__(self, nombre, descripcion_consulta):
-        self.nombre = nombre
-        self.descripcion_consulta = descripcion_consulta
-        self.prioridad = self.calcular_prioridad()  # Calculamos la prioridad cuando se crea el paciente
+    def __init__(self, nombre:str, descripcion_consulta:str)->None:
+        self.nombre:str = nombre
+        self.descripcion_consulta:str = descripcion_consulta
+        self.prioridad:int = self.calcular_prioridad() 
 
-    def calcular_prioridad(self):
-        # Asigna prioridad según palabras clave en la descripción
+    def calcular_prioridad(self)->int:
         if any(word in self.descripcion_consulta.lower() for word in ["dolor agudo", "fractura", "ataque"]):
             return 5
         elif any(word in self.descripcion_consulta.lower() for word in ["fiebre", "tos"]):
             return 3
         elif any(word in self.descripcion_consulta.lower() for word in ["revisión", "control"]):
             return 1
-        return 1  # Prioridad por defecto si no se encuentra palabra clave
+        return 1  
 
-    def __str__(self):
+    def __str__(self)->str:
         return f"Paciente({self.nombre}, Prioridad: {self.prioridad}, Consulta: {self.descripcion_consulta})"
