f1c58240173875fcf86a3876cc9b570c8fdf4505
alejogsz <140736259+alejogsz@users.noreply.github.com>
2024-11-07 09:15:15 -0500

    Update colaprioridad.py
    eliminación comentarios y se añade typehints

--- listasenlazadas/colaprioridad.py
+++ listasenlazadas/colaprioridad.py
@@ -1,41 +1,37 @@
 from nodo import Nodo
-
+from paciente import Paciente 
 
 
 class ColaPrioridad:
-    def __init__(self):
-        self.head = None  # La cabeza de la lista enlazada, que representa el paciente con mayor prioridad
+    def __init__(self)->None:
+        self.head:Nodo = None  
 
-    def insertar(self, paciente):
-        nuevo_nodo = Nodo(paciente)
+    def insertar(self, paciente:Paciente)->None:
+        nuevo_nodo:Nodo = Nodo(paciente)
 
-        # Si la lista está vacía o el paciente tiene la prioridad más alta, se coloca al inicio
         if self.head is None or self.head.dato.prioridad < paciente.prioridad:
             nuevo_nodo.siguiente = self.head
-            self.head = nuevo_nodo
+            self.head:Nodo = nuevo_nodo
         else:
-            # Recorremos la lista para encontrar la posición correcta
-            actual = self.head
+
+            actual:Nodo = self.head
             while actual.siguiente is not None and actual.siguiente.dato.prioridad >= paciente.prioridad:
                 actual = actual.siguiente
 
-            # Insertamos el nuevo nodo en la posición encontrada
             nuevo_nodo.siguiente = actual.siguiente
             actual.siguiente = nuevo_nodo
 
-    def atender(self):
-        # Atiende (remueve) al paciente con mayor prioridad
+    def atender(self)->str:
         if self.head is None:
             print("No hay pacientes en espera.")
             return None
 
         paciente_a_atender = self.head.dato
-        self.head = self.head.siguiente  # Avanzamos la cabeza al siguiente nodo
+        self.head = self.head.siguiente 
         print(f"Atendiendo a: {paciente_a_atender}")
         return paciente_a_atender
 
-    def mostrar(self):
-        # Muestra todos los pacientes en la cola de prioridad
+    def mostrar(self)->str:
         if self.head is None:
             print("La cola de pacientes está vacía.")
             return
